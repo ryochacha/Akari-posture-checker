58dbc666e0432b2cd0f5413698552e49c024cbb3
ryochacha <ryoei.y.06.26@gmail.com>
2025-06-26 11:28:26 +0900

    main

--- main.py
+++ main.py
@@ -33,30 +33,12 @@ def main() -> None:
    # akari_client_configを引数にしてAkariClientを作成する。
    akari = AkariClient(akari_client_config)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if person_detected:
-            # --- 2. 姿勢を分析 ---
-            feedback, is_stooped = analyzer.analyze_posture(
-                body, angle_threshold=cfg.ANGLE_THRESHOLD
-            )
-            standing = analyzer.is_standing(
-                body, knee_angle_threshold=cfg.STANDING_KNEE_ANGLE_THRESHOLD
-            )
-=======
-=======
->>>>>>> parent of f6d47e2 (Merge pull request #1 from ryochacha/akari_my_branch)
    # 処理を記載。下記は例
    joints = akari.joints
    # サーボトルクをONする。
    joints.enable_all_servo()
    # 初期位置に移動する。
-<<<<<<< HEAD
-   joints.move_joint_positions(sync=True, pan=0, tilt=0)
->>>>>>> parent of efd76f7 (main更新)
-=======
-   joints.move_joint_positions(sync=True, pan=0.4, tilt=0)
->>>>>>> parent of f6d47e2 (Merge pull request #1 from ryochacha/akari_my_branch)
+   joints.move_joint_positions(sync=True, pan=1, tilt=0)
 
 
 if __name__ == "__main__":
